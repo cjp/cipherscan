239f4879fc3523826a796a25dc3d348ddc10c295
Christopher J. Pilkington <cjp@0x1.net>
2012-02-17 14:51:31 +0000

    Added checking for openssl.

--- cipherscan.sh
+++ cipherscan.sh
@@ -2,20 +2,24 @@
 #
 # cipherscan.sh -- Test an SSL/TLS server for cipher suite acceptance.
 #
-# $Id$
-# $Source$
+# $Id: cipherscan.sh,v 1.1 2012/02/16 17:14:46 cjp Exp $
+# $Source: /home/cjp/RCS/cipherscan.sh,v $
 #
-if [ $# -ne 2 ]
-then
-  echo "Usage: `basename $0` {hostname} {port}"
+if [ $# -ne 2 ]; then
+  echo >&2 "Usage: `basename $0` {hostname} {port}"
   exit 65;
 fi
+
+hash openssl &>-
+if [ $? -eq 1 ]; then
+  echo >&2 "`basename $0` requires openssl."
+  exit 65;
+fi 
 
 hostname=$1;
 port=$2;
 
-if echo "" | nc $hostname $port 2&>1 >/dev/null
-then 
+if echo "" | nc $hostname $port 2&>1 >/dev/null; then
   echo "Testing $hostname, port $port."
 
   for cipher in `openssl ciphers -v 'ALL:eNULL' | cut -d' ' -f1`; do
@@ -26,6 +30,9 @@ then
     echo -en "$?\t$cipher\n";
   done
 else
-  echo "Error connecting to $hostname on port $port."
+  echo >&2 "Error connecting to $hostname on port $port."
   exit 65;
 fi
+#
+# $Log$
+#
