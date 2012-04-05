b4e4952faf228dd9ce531858bd516e32a067c734
Christopher J. Pilkington <cjp@0x1.net>
2012-04-05 15:16:19 -0400

    Remove RCS vestiges.

--- cipherscan.sh
+++ cipherscan.sh
@@ -2,9 +2,6 @@
 #
 # cipherscan.sh -- Test an SSL/TLS server for cipher suite acceptance.
 #
-# $Id: cipherscan.sh,v 1.3 2012/04/05 16:42:38 cjp Exp $
-# $Source: /home/cjp/RCS/cipherscan.sh,v $
-#
 if [ $# -ne 2 ]; then
   printf >&2 "Usage: `basename %s` {hostname} {port}\n" "$0"
   exit 65;
@@ -34,12 +31,3 @@ for cipher in `openssl ciphers -v 'ALL:eNULL' | cut -d' ' -f1`; do
                         3>&1 1>/dev/null 2>&3 | grep -En 'handshake failure|no ciphers available|errno=104' >/dev/null;
   printf "%s\t%s\n" "$?" "$cipher";
 done
-#
-# $Log: cipherscan.sh,v $
-# Revision 1.3  2012/04/05 16:42:38  cjp
-# Changed error detection strings to work in more situations.
-#
-# Revision 1.2  2012/02/17 14:51:31  cjp
-# Added checking for openssl.
-#
-#
