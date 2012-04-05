89894b62ace2ff024c54e46d5453f522b120c3a4
Christopher J. Pilkington <cjp@0x1.net>
2012-04-05 16:42:38 +0000

    Changed error detection strings to work in more situations.

--- cipherscan.sh
+++ cipherscan.sh
@@ -2,7 +2,7 @@
 #
 # cipherscan.sh -- Test an SSL/TLS server for cipher suite acceptance.
 #
-# $Id: cipherscan.sh,v 1.1 2012/02/16 17:14:46 cjp Exp $
+# $Id: cipherscan.sh,v 1.2 2012/02/17 14:51:31 cjp Exp cjp $
 # $Source: /home/cjp/RCS/cipherscan.sh,v $
 #
 if [ $# -ne 2 ]; then
@@ -19,20 +19,24 @@ fi
 hostname=$1;
 port=$2;
 
-if echo "" | nc $hostname $port 2&>1 >/dev/null; then
-  echo "Testing $hostname, port $port."
-
-  for cipher in `openssl ciphers -v 'ALL:eNULL' | cut -d' ' -f1`; do
-    echo -e "\n" | \
-      openssl s_client -cipher "-ALL:$cipher" \
-                       -connect $hostname:$port \
-                          3>&1 1>/dev/null 2>&3 | grep -n error >/dev/null;
-    echo -en "$?\t$cipher\n";
-  done
-else
+echo "" | nc -w3 $hostname $port 2&>1 >/dev/null
+if [ $? -eq 1 ]; then
   echo >&2 "Error connecting to $hostname on port $port."
   exit 65;
 fi
+
+echo "Testing $hostname, port $port."
+
+for cipher in `openssl ciphers -v 'ALL:eNULL' | cut -d' ' -f1`; do
+  echo -n "" | \
+    openssl s_client -cipher "-ALL:$cipher" \
+                     -connect $hostname:$port \
+                        3>&1 1>/dev/null 2>&3 | grep -En 'handshake failure|no ciphers available|errno=104' >/dev/null;
+  echo -en "$?\t$cipher\n";
+done
 #
-# $Log$
+# $Log: cipherscan.sh,v $
+# Revision 1.2  2012/02/17 14:51:31  cjp
+# Added checking for openssl.
+#
 #
